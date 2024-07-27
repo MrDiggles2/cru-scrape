3369ec733edaa25849c57a55f94bee65057297a9
Chan Lee <chan.lee@umbrage.com>
2024-07-27 01:29:56 +0000

    idk

--- src/main.py
+++ src/main.py
@@ -2,29 +2,37 @@ from scrapy.crawler import CrawlerProcess
 from get_yearly_snapshot import get_yearly_snapshot
 from spider import Spider
 import time
+from twisted.internet import defer, reactor
+
 
 baseUrls = [
   "https://www.maine.gov/ifw/"
 ]
 
-for baseUrl in baseUrls:
-  urls = get_yearly_snapshot(baseUrl)
 
-  for url in urls:
+@defer.inlineCallbacks
+def crawl():
+  for baseUrl in baseUrls:
+    urls = get_yearly_snapshot(baseUrl)
 
-    print(f'Starting {url}')
+    for url in urls:
 
-    process = CrawlerProcess(settings={
-        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'ITEM_PIPELINES': {
-            'pipeline.MyPipeline': 300,
-        },
-        "DOWNLOAD_DELAY": "1.0",
-        "CONCURRENT_REQUESTS_PER_DOMAIN": "1"
-    })
+      print(f'Starting {url}')
 
-    process.crawl(Spider, url)
+      process = CrawlerProcess(settings={
+          'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+          'ITEM_PIPELINES': {
+              'pipeline.MyPipeline': 300,
+          },
+          "DOWNLOAD_DELAY": "1.0",
+          "CONCURRENT_REQUESTS_PER_DOMAIN": "1"
+      })
 
-    start = time.time()
-    process.start()
-    print(f'Took {time.time()- start} seconds')
+      start = time.time()
+      yield process.crawl(Spider, url)
+
+      print(f'Took {time.time()- start} seconds')
+    reactor.stop()
+
+crawl()
+reactor.run()
