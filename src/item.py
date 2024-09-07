f26c8c8b7ed74e9e709c29dfdceb97830b22afea
Chan Lee <chan.lee@umbrage.com>
2024-09-07 21:06:57 +0000

    removes unused code

--- src/item.py
+++
@@ -1,6 +0,0 @@
-import scrapy
-
-class MyItem(scrapy.Item): 
-  url = scrapy.Field()
-  content = scrapy.Field()
-  base_url = scrapy.Field()
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,35 +1,7 @@
-import json
-import time
-import os
 import logging
 
 from src.entities import PageItem
-from src.item import MyItem
 from src.utils.psql import get_connection, upsert_page
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-output_path = os.path.join(dir_path, '../output')
-
-class MyPipeline:
-    file: str
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.file = f'{output_path}/{int(time.time())}.ndjson'
-
-    def process_item(self, item: MyItem, spider):
-        with open(self.file, 'a') as f:
-            f.write(
-                json.dumps({
-                    'url': item['url'],
-                    'base_url': item['base_url'],
-                    'content': item['content']
-                }) + '\n'
-            )
-
-            print('Wrote to ${self.file}')
-
-        return item
 
 class DbPipeline:
     def __init__(self, *args, **kwargs):
