bf30f3fa0aa334cfab433d35e116c4f7b07977f1
Chan Lee <chan.lee@umbrage.com>
2024-09-24 15:21:35 +0000

    fixes stale connection issues with db pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,20 +9,15 @@ class DbPipeline:
         super().__init__(*args, **kwargs)
         self.logger = logging.getLogger("dbpipeline")
 
-    def open_spider(self, spider):
-        self.conn = get_connection()
-
-    def close_spider(self, spider):
-        self.conn.close()
-
     def process_item(self, item: PageItem, spider):
-        try:
-            page_id = upsert_page(self.conn, item)
-            self.logger.info(f'updated page {page_id}')
-        except Exception as e:
-            logging.error(e)
-        else:
-            self.conn.commit()
+        with get_connection() as conn:
+            try:
+                page_id = upsert_page(conn, item)
+                self.logger.info(f'updated page {page_id}')
+            except Exception as e:
+                logging.error(e)
+            else:
+                conn.commit()
 
 class StdoutPipeline:
     def __init__(self, *args, **kwargs):
